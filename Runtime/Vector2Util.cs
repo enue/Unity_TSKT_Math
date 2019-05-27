18008d42ffa636d030af81a76fb267ee8262aeb6
enue <nnp@car.ocn.ne.jp>
2019-05-27 09:24:59 +0900

    add Vector2Util

---
+++ Runtime/Vector2Util.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace TSKT
+{
+    public static class Vector2Util
+    {
+        static public Vector2 Rotate(Vector2 source, float radian)
+        {
+            var sin = Mathf.Sin(radian);
+            var cos = Mathf.Cos(radian);
+
+            return new Vector2(
+                cos * source.x - sin * source.y,
+                sin * source.x + cos * source.y);
+        }
+    }
+}

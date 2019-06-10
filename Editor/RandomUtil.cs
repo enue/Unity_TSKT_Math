ef498f491d6949c8718add43e47ce6e54874839f
enue <nnp@car.ocn.ne.jp>
2019-06-10 18:15:45 +0900

    0.4.0  RandomUtil

---
+++ Editor/RandomUtil.cs
@@ -0,0 +1,55 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using NUnit.Framework;
+using System.Linq;
+
+namespace TSKT.Tests
+{
+    public class RandomUtil
+    {
+        [Test]
+        [TestCase(0, 10)]
+        [TestCase(100, 10)]
+        public void GenerateShuffledRange(int start, int count)
+        {
+            var original = Enumerable.Range(start, count);
+            var shuffled = TSKT.RandomUtil.GenerateShuffledRange(start, count);
+            Assert.True(original.All(_ => shuffled.Contains(_)));
+        }
+
+        [Test]
+        [TestCase(1, 2, 3, 4, 5, 6, 7, 8, 9)]
+        public void GenerateShuffledArray(params object[] parameters)
+        {
+            var original = new List<object>(parameters);
+            var shuffled = TSKT.RandomUtil.GenerateShuffledArray(original);
+            Assert.True(original.All(_ => shuffled.Contains(_)));
+        }
+
+        [Test]
+        [TestCase(1, 2, 3, 4, 5, 6, 7, 8, 9)]
+        public void ShuffleList(params object[] parameters)
+        {
+            var original = new List<object>(parameters);
+            var shuffled = new List<object>(original);
+            TSKT.RandomUtil.Shuffle(ref shuffled);
+            Assert.True(original.All(_ => shuffled.Contains(_)));
+        }
+
+        [Test]
+        [TestCase(1, 2, 3, 4, 5, 6, 7, 8, 9)]
+        public void ShuffleArray(params object[] parameters)
+        {
+            var shuffled = new object[parameters.Length];
+            for(int i=0; i<parameters.Length; ++i)
+            {
+                shuffled[i] = parameters[i];
+            }
+
+            TSKT.RandomUtil.Shuffle(ref shuffled);
+            Assert.True(parameters.All(_ => shuffled.Contains(_)));
+        }
+    }
+}
+
---
+++ Runtime/RandomUtil.cs
@@ -0,0 +1,55 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using System.Security.Cryptography;
+using Unity.Collections;
+
+namespace TSKT
+{
+    static public class RandomUtil
+    {
+        static public int[] GenerateShuffledRange(int start, int count)
+        {
+            var result = new int[count];
+            for (int i = 0; i < count; ++i)
+            {
+                result[i] = i + start;
+            }
+            Shuffle(ref result);
+            return result;
+        }
+
+        static public T[] GenerateShuffledArray<T>(List<T> list)
+        {
+            var result = new T[list.Count];
+            for(int i=0; i<result.Length; ++i)
+            {
+                result[i] = list[i];
+            }
+            Shuffle(ref result);
+            return result;
+        }
+
+        static public void Shuffle<T>(ref List<T> list)
+        {
+            for(int i=0; i < list.Count - 1; ++i)
+            {
+                var swapIndex = Random.Range(i, list.Count);
+                var t = list[i];
+                list[i] = list[swapIndex];
+                list[swapIndex] = t;
+            }
+        }
+
+        static public void Shuffle<T>(ref T[] list)
+        {
+            for (int i = 0; i < list.Length - 1; ++i)
+            {
+                var swapIndex = Random.Range(i, list.Length);
+                var t = list[i];
+                list[i] = list[swapIndex];
+                list[swapIndex] = t;
+            }
+        }
+    }
+}

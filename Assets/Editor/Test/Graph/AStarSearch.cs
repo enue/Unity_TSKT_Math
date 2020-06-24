3dec621d5b544eb66dc17e329022a83487c55ef8
enu <otoriden@gmail.com>
2020-06-24 13:33:53 +0900

    add AStarSearch

---
+++ Assets/Editor/Test/Graph/AStarSearch.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using NUnit.Framework;
+using System.Linq;
+
+namespace TSKT.Tests
+{
+    public class AStarSearch
+    {
+        [Test]
+        [TestCase(100, 100, 1f)]
+        [TestCase(10, 10, 0f)]
+        public void Test(int width, int height, float costRandom)
+        {
+            var start = new Vector2Int(UnityEngine.Random.Range(0, width), UnityEngine.Random.Range(0, height));
+            var goal = new Vector2Int(UnityEngine.Random.Range(0, width), UnityEngine.Random.Range(0, height));
+            var board = new Board(width, height);
+            if (costRandom > 0f)
+            {
+                for (int i = 0; i < width; ++i)
+                {
+                    for (int j = 0; j < height; ++j)
+                    {
+                        board.SetCost(i, j, 1.0 + UnityEngine.Random.Range(0f, costRandom));
+                    }
+                }
+            }
+
+            var distanceMap = board.ComputeDistancesFrom(start);
+            var dijkstraRoutes = distanceMap.ComputeRoutesToPivotFrom(goal).ToArray();
+            var goalDistanceByDijkstra = distanceMap.Distances[goal];
+
+            var aStarSearch = new AStarSearch<Vector2Int>(board, start, goal, (a, b) => TSKT.Vector2IntUtil.GetManhattanDistance(a, b));
+            var aStarPath = aStarSearch.ComputePathFromGoalToStart();
+            var goalDistanceByAStarSearch = aStarSearch.Distances[goal];
+            Assert.AreEqual(goalDistanceByDijkstra, goalDistanceByAStarSearch);
+            Assert.IsTrue(dijkstraRoutes.Any(_ => _.SequenceEqual(aStarPath)));
+        }
+    }
+}
---
+++ Assets/Package/Runtime/Graph/AStarSearch.cs
@@ -0,0 +1,140 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Linq;
+
+namespace TSKT
+{
+    public readonly struct AStarSearch<T>
+    {
+        class PriorityQueue
+        {
+            public readonly struct Key
+            {
+                public class Comparer : IComparer<Key>
+                {
+                    public int Compare(Key x, Key y)
+                    {
+                        if (x.primary > y.primary)
+                        {
+                            return 1;
+                        }
+                        if (x.primary < y.primary)
+                        {
+                            return -1;
+                        }
+                        if (x.secondary > y.secondary)
+                        {
+                            return 1;
+                        }
+                        if (x.secondary < y.secondary)
+                        {
+                            return -1;
+                        }
+                        return 0;
+                    }
+                }
+
+                public readonly double primary;
+                public readonly double secondary;
+
+                public Key(double primary, double secondary)
+                {
+                    this.primary = primary;
+                    this.secondary = secondary;
+                }
+            }
+
+            readonly List<T> items = new List<T>();
+            readonly List<Key> keys = new List<Key>();
+            readonly Key.Comparer keyComparer = new Key.Comparer();
+            public int Count => keys.Count;
+
+            public void Enqueue(double primaryKey, double secondaryKey, T item)
+            {
+                // Dequeueの時に末尾からとりたいのでキーをマイナスにしておく
+                var key = new Key(-primaryKey, -secondaryKey);
+                var index = keys.BinarySearch(key, keyComparer);
+                if (index < 0)
+                {
+                    index = ~index;
+                }
+                items.Insert(index, item);
+                keys.Insert(index, key);
+            }
+
+            public (double primaryKey, double secondaryKey, T item) Dequeue()
+            {
+                var index = items.Count - 1;
+                var item = items[index];
+                var key = keys[index];
+                items.RemoveAt(index);
+                keys.RemoveAt(index);
+                return (-key.primary, -key.secondary, item);
+            }
+        }
+
+        readonly DistanceMap<T> distanceMap;
+        public T Start => distanceMap.Pivot;
+        public Dictionary<T, double> Distances => distanceMap.Distances;
+        public Dictionary<T, (T node, double distance)[]> Edges => distanceMap.Edges;
+
+        public T Goal { get; }
+
+        public AStarSearch(IGraph<T> graph, T start, T goal, System.Func<T, T, double> heuristicFunction)
+        {
+            Goal = goal;
+            distanceMap = new DistanceMap<T>(
+                start,
+                new Dictionary<T, double>(),
+                new Dictionary<T, (T node, double distance)[]>());
+            Distances.Add(start, 0.0);
+
+            var tasks = new PriorityQueue();
+            tasks.Enqueue(heuristicFunction(start, goal), 0.0, start);
+
+            while (tasks.Count > 0)
+            {
+                var (expectedDistance, _, currentNode) = tasks.Dequeue();
+                if (Distances.TryGetValue(Goal, out var startToGoalDistance))
+                {
+                    if (expectedDistance >= startToGoalDistance)
+                    {
+                        continue;
+                    }
+                }
+
+                if (!Edges.TryGetValue(currentNode, out var nexts))
+                {
+                    nexts = graph.GetNextNodeDistancesFrom(currentNode)?.ToArray();
+                    Edges.Add(currentNode, nexts);
+                }
+
+                var startToCurrentNodeDistance = Distances[currentNode];
+
+                foreach (var (next, edgeWeight) in nexts)
+                {
+                    var startToNextNodeDistance = edgeWeight + startToCurrentNodeDistance;
+
+                    if (Distances.TryGetValue(next, out var oldDistance))
+                    {
+                        if (oldDistance <= startToNextNodeDistance)
+                        {
+                            continue;
+                        }
+                    }
+
+                    Distances[next] = startToNextNodeDistance;
+
+                    var nextExpectedDistance = heuristicFunction(next, goal) + startToNextNodeDistance;
+                    // nextExpectedDistanceは昇順、startToNextNodeDistanceは降順で処理する
+                    tasks.Enqueue(nextExpectedDistance, -startToNextNodeDistance, next);
+                }
+            }
+        }
+
+        public Dictionary<T, List<T>> EdgesToStart => distanceMap.EdgesToPivot;
+        public List<T> ComputePathFromGoalToStart() => distanceMap.ComputeRoutesToPivotFrom(Goal).FirstOrDefault();
+        public bool FoundPath => Distances.ContainsKey(Goal);
+    }
+}

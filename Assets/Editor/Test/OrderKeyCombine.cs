05552fbb0fc0b94dfc054cf322903b83c34b3458
enu <otoriden@gmail.com>
2020-07-15 05:36:13 +0900

    OrderKeyCombine

---
+++ Assets/Editor/Test/OrderKeyCombine.cs
@@ -0,0 +1,94 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using NUnit.Framework;
+using System.Linq;
+
+namespace TSKT.Tests
+{
+    public class OrderKeyCombineTest
+    {
+        [Test]
+        public void DoubleCapacity()
+        {
+            var combine = new OrderKeyCombine();
+            combine.Append(double.MaxValue);
+            Assert.AreEqual(OrderKeyCombine.capacity, combine.Length);
+            Assert.Catch(() => combine.Append(double.MaxValue));
+            Assert.Catch(() => combine.Append(true));
+        }
+        [Test]
+        public void ByteCapacity()
+        {
+            var combine = new OrderKeyCombine();
+            combine.Append(byte.MaxValue);
+            combine.Append(byte.MaxValue);
+            combine.Append(byte.MaxValue);
+            combine.Append(byte.MaxValue);
+            combine.Append(byte.MaxValue);
+            combine.Append(byte.MaxValue);
+            combine.Append(byte.MaxValue);
+            combine.Append(byte.MaxValue);
+            Assert.AreEqual(ulong.MaxValue, combine.Result);
+            Assert.AreEqual(OrderKeyCombine.capacity, combine.Length);
+            Assert.Catch(() => combine.Append(true));
+        }
+        [Test]
+        public void UshortCapacity()
+        {
+            var combine = new OrderKeyCombine();
+            combine.Append(ushort.MaxValue);
+            combine.Append(ushort.MaxValue);
+            combine.Append(ushort.MaxValue);
+            combine.Append(ushort.MaxValue);
+            Assert.AreEqual(ulong.MaxValue, combine.Result);
+            Assert.AreEqual(OrderKeyCombine.capacity, combine.Length);
+            Assert.Catch(() => combine.Append(true));
+        }
+
+        [Test]
+        public void UintCapacity()
+        {
+            var combine = new OrderKeyCombine();
+            combine.Append(uint.MaxValue);
+            combine.Append(uint.MaxValue);
+            Assert.AreEqual(ulong.MaxValue, combine.Result);
+            Assert.AreEqual(OrderKeyCombine.capacity, combine.Length);
+            Assert.Catch(() => combine.Append(true));
+        }
+
+        [Test]
+        public void IntCapacity()
+        {
+            var combine = new OrderKeyCombine();
+            combine.Append(int.MaxValue);
+            combine.Append(int.MaxValue);
+            Assert.AreEqual(ulong.MaxValue, combine.Result);
+            Assert.AreEqual(OrderKeyCombine.capacity, combine.Length);
+            Assert.Catch(() => combine.Append(true));
+        }
+
+        [Test]
+        public void FloatCapacity()
+        {
+            var combine = new OrderKeyCombine();
+            combine.Append(float.MaxValue);
+            combine.Append(float.MaxValue);
+            Assert.AreEqual(OrderKeyCombine.capacity, combine.Length);
+            Assert.Catch(() => combine.Append(true));
+        }
+
+        [Test]
+        public void BoolCapacity()
+        {
+            var combine = new OrderKeyCombine();
+            for (int i = 0; i < 64; ++i)
+            {
+                combine.Append(true);
+            }
+            Assert.AreEqual(ulong.MaxValue, combine.Result);
+            Assert.AreEqual(OrderKeyCombine.capacity, combine.Length);
+            Assert.Catch(() => combine.Append(true));
+        }
+    }
+}
---
+++ Assets/Package/Runtime/OrderKeyCombine.cs
@@ -0,0 +1,80 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace TSKT
+{
+    public class OrderKeyCombine
+    {
+        public ulong Result { get; private set; }
+        public int Length { get; private set; }
+        public const int capacity = 64;
+
+        public void Clear()
+        {
+            Result = 0;
+            Length = 0;
+        }
+
+        public void Append(double k)
+        {
+            if (Length > 0)
+            {
+                throw new System.ArgumentException();
+            }
+            var v = OrderKeyConvert.ToUint64(k);
+            Result = v;
+            Length = 64;
+        }
+
+        public void Append(float k)
+        {
+            var v = OrderKeyConvert.ToUint32(k);
+            Append(v);
+        }
+        public void Append(int k)
+        {
+            var v = OrderKeyConvert.ToUint32(k);
+            Append(v);
+        }
+
+        public void Append(uint k)
+        {
+            if (Length > capacity - 32)
+            {
+                throw new System.ArgumentException();
+            }
+            Result += (ulong)k << (capacity - Length - 32);
+            Length += 32;
+        }
+
+        public void Append(bool k)
+        {
+            if (Length > capacity - 1)
+            {
+                throw new System.ArgumentException();
+            }
+            Result += (ulong)(k ? 1 : 0) << (capacity - Length - 1);
+            Length += 1;
+        }
+
+        public void Append(byte k)
+        {
+            if (Length > capacity - 8)
+            {
+                throw new System.ArgumentException();
+            }
+            Result += (ulong)k << (capacity - Length - 8);
+            Length += 8;
+        }
+        public void Append(ushort k)
+        {
+            if (Length > capacity - 16)
+            {
+                throw new System.ArgumentException();
+            }
+            Result += (ulong)k << (capacity - Length - 16);
+            Length += 16;
+        }
+    }
+}

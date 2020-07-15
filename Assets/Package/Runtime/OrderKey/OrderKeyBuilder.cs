2e8bc1ed7e03c1046eba906b5a51b144e05c5572
enu <otoriden@gmail.com>
2020-07-15 13:22:56 +0900

    OrderKeyCombone.Append(key, out ulong filled)

--- Assets/Package/Runtime/OrderKey/OrderKeyBuilder.cs
+++ Assets/Package/Runtime/OrderKey/OrderKeyBuilder.cs
@@ -6,68 +6,63 @@ namespace TSKT
 {
     public class OrderKeyBuilder
     {
-        readonly List<OrderKeyCombine> items = new List<OrderKeyCombine>();
-
-        void AppendUint64(ulong key, int index, int count)
-        {
-            if (items.Count == 0)
-            {
-                items.Add(new OrderKeyCombine());
-            }
-            var last = items[items.Count - 1];
-            var remainingCapacity = OrderKeyCombine.capacity - last.Length;
-            var capacityOverCount = count - remainingCapacity;
-            if (capacityOverCount > 0)
-            {
-                last.Append(key, index, remainingCapacity);
-                items[items.Count - 1] = last;
-
-                var newItem = new OrderKeyCombine();
-                newItem.Append(key, index + remainingCapacity, capacityOverCount);
-                items.Add(newItem);
-            }
-            else
-            {
-                last.Append(key, index, count);
-                items[items.Count - 1] = last;
-            }
-        }
+        OrderKeyCombine combine = new OrderKeyCombine();
+        readonly List<ulong> keys = new List<ulong>();
 
         public void AppendUint64(ulong key)
         {
-            AppendUint64(key, 0, 64);
+            if (combine.Append(key, out var filledKey))
+            {
+                keys.Add(filledKey);
+            }
         }
 
         public void AppendUint32(uint key)
         {
-            AppendUint64(key, 64 - 32, 32);
+            if (combine.Append(key, out var filledKey))
+            {
+                keys.Add(filledKey);
+            }
         }
 
         public void AppendUint16(ushort key)
         {
-            AppendUint64(key, 64 - 16, 16);
+            if (combine.Append(key, out var filledKey))
+            {
+                keys.Add(filledKey);
+            }
         }
 
         public void AppendUint8(byte key)
         {
-            AppendUint64(key, 64 - 8, 8);
+            if (combine.Append(key, out var filledKey))
+            {
+                keys.Add(filledKey);
+            }
         }
 
         public void AppendBool(bool key)
         {
-            AppendUint64((ulong)(key ? 1 : 0), 64 - 1, 1);
+            if (combine.Append(key, out var filledKey))
+            {
+                keys.Add(filledKey);
+            }
         }
 
         public OrderKey3 ToOrderKey3()
         {
-            if (items.Count > 3)
+            if (keys.Count > 3)
+            {
+                throw new System.Exception();
+            }
+            if (keys.Count == 3 && combine.Length > 0)
             {
                 throw new System.Exception();
             }
             return new OrderKey3(
-                items.Count > 0 ? items[0].Result : 0,
-                items.Count > 1 ? items[1].Result : 0,
-                items.Count > 2 ? items[2].Result : 0);
+                keys.Count > 0 ? keys[0] : keys.Count == 0 ? combine.Result : 0,
+                keys.Count > 1 ? keys[1] : keys.Count == 1 ? combine.Result : 0,
+                keys.Count > 2 ? keys[2] : keys.Count == 2 ? combine.Result : 0);
         }
     }
 }
--- Assets/Package/Runtime/OrderKey/OrderKeyCombine.cs
+++ Assets/Package/Runtime/OrderKey/OrderKeyCombine.cs
@@ -94,5 +94,67 @@ namespace TSKT
             Result |= (ulong)k << (capacity - Length - 16);
             Length += 16;
         }
+
+        bool Append(ulong k, int index, int count, out ulong filled)
+        {
+            var remainingCapacity = capacity - Length;
+            if (remainingCapacity >= count)
+            {
+                Append(k, index, count);
+                filled = default;
+                return false;
+            }
+
+            Append(k, index, remainingCapacity);
+            filled = Result;
+
+            Result = 0;
+            Length = 0;
+            Append(k, index + remainingCapacity, count - remainingCapacity);
+
+            return true;
+        }
+
+        public bool Append(ulong k, out ulong filled)
+        {
+            return Append(k, 0, 64, out filled);
+        }
+
+        public bool Append(uint k, out ulong filled)
+        {
+            return Append(k, 32, 32, out filled);
+        }
+
+        public bool Append(ushort k, out ulong filled)
+        {
+            return Append(k, 64 - 16, 16, out filled);
+        }
+
+        public bool Append(byte k, out ulong filled)
+        {
+            return Append(k, 64 - 8, 8, out filled);
+        }
+
+        public bool Append(bool k, out ulong filled)
+        {
+            return Append(k ? 1UL : 0UL, 64 - 1, 1, out filled);
+        }
+
+        public bool Append(double k, out ulong filled)
+        {
+            var v = OrderKeyConvert.ToUint64(k);
+            return Append(v, out filled);
+        }
+        public bool Append(float k, out ulong filled)
+        {
+            var v = OrderKeyConvert.ToUint32(k);
+            return Append(v, out filled);
+        }
+        public bool Append(int k, out ulong filled)
+        {
+            var v = OrderKeyConvert.ToUint32(k);
+            return Append(v, out filled);
+        }
+
     }
 }

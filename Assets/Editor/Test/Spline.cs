e18591631b3d92ae61c6d1e0b1fd74321371e1a6
enue <otoriden@gmail.com>
2022-03-26 09:18:10 +0900

    wip Spline

--- Assets/Editor/Test/Spline.cs
+++ Assets/Editor/Test/Spline.cs
@@ -9,86 +9,46 @@ namespace TSKT.Tests
     public class Spline
     {
         [Test]
-        [TestCase(1)]
         [TestCase(2)]
         [TestCase(3)]
         [TestCase(4)]
         [TestCase(10)]
+        [TestCase(100)]
         public void SolvePoints(int pointCount)
         {
-            var points = new List<(double time, double value)>();
+            var points = new List<(float time, float value)>();
             var endTime = 0f;
             for (int i = 0; i < pointCount; ++i)
             {
                 endTime += Random.value;
                 points.Add((endTime, Random.value));
             }
-            var spline = TSKT.Spline.SolvePoints(points.ToArray());
+            var spline = new TSKT.Spline(points.ToArray());
             Validate(spline, points);
         }
 
-        [Test]
-        [TestCase(2)]
-        [TestCase(3)]
-        [TestCase(4)]
-        [TestCase(10)]
-        public void SolveVelocityAndPoints(int pointCount)
+        static void Validate(TSKT.Spline spline, List<(float time, float value)> points)
         {
-            for (int i = 0; i < pointCount; ++i)
+            for (int j = 0; j < spline.EndTimes.Length - 1; ++j)
             {
-                var points = new List<(double time, double value)>();
-                var endTime = 0f;
-                for (int j = 0; j < pointCount; ++j)
-                {
-                    endTime += Random.value;
-                    points.Add((endTime, Random.value));
-                }
-                var tt = Mathf.Lerp(-1, endTime + 1f, (float)i / pointCount);
-                var spline = TSKT.Spline.SolvedVelocityAndPoints(tt, Random.value, points.ToArray());
-                Validate(spline, points);
-            }
-        }
-
-        [Test]
-        [TestCase(2)]
-        [TestCase(3)]
-        [TestCase(6)]
-        [TestCase(10)]
-        public void SolveAccelAndVelocityAndPoints(int pointCount)
-        {
-            var points = new List<(double time, double value)>();
-            var endTime = 0f;
-            for (int i = 0; i < pointCount; ++i)
-            {
-                endTime += 1f;
-                points.Add((endTime, Random.value));
-            }
-
-            for (int i = 0; i < points.Count + 3; ++i)
-            {
-                var tt = Mathf.Lerp(-1, endTime + 1f, (float)i / points.Count);
-                var a = Random.value;
-                var v = Random.value;
-                var spline = TSKT.Spline.SolveAccelAndVelocityAndPoints(tt, a, v, points.ToArray());
-                Validate(spline, points);
-                Assert.AreEqual(spline.SortedKeys.ToArray(), points.Skip(1).Select(_ => _.time).ToArray());
-                Assert.AreEqual(spline.SortedKeys.ToArray(), spline.SortedKeys.ToArray().OrderBy(_ => _).ToArray());
-                Assert.AreEqual(points.Count - 1, spline.SortedKeys.Length);
-            }
-        }
-
-        static void Validate(TSKT.Spline spline, List<(double time, double value)> points)
-        {
-            for (int j = 0; j < spline.SortedKeys.Length - 1; ++j)
-            {
-                var t = spline.SortedKeys[j];
-                var f1 = spline.Functions[j];
-                var f2 = spline.Functions[j + 1];
+                var t = spline.EndTimes[j];
+                var f1 = spline.Intervals[j];
+                var f2 = spline.Intervals[j + 1];
                 Assert.AreEqual(f1.Evaluate(t), f2.Evaluate(t), 0.00001);
             }
             foreach (var it in points)
             {
                 Assert.AreEqual(it.value, spline.Evaluate(it.time), 0.00001);
+            }
+            for (int i = 0; i < spline.EndTimes.Length - 1; i++)
+            {
+                var a = spline.Intervals[i];
+                var b = spline.Intervals[i + 1];
+                var t = spline.EndTimes[i];
+
+                Assert.AreEqual(a.Evaluate(t), b.Evaluate(t));
+                Assert.AreEqual(a.Acceleration(t), b.Acceleration(t));
+                Assert.AreEqual(a.Velocity(t), b.Velocity(t));
             }
         }
     }
--- Assets/Package/Runtime/Spline.cs
+++ Assets/Package/Runtime/Spline.cs
@@ -1,199 +1,306 @@
-﻿#nullable enable
-using System;
-using System.Collections;
+﻿using System.Collections;
 using System.Collections.Generic;
-using System.Linq;
 using UnityEngine;
+using Unity.Mathematics;
+using System.Linq;
+
 
 namespace TSKT
 {
-    public readonly struct Spline
+    public class Spline
     {
-        readonly double[] sortedKeys;
-        public ReadOnlySpan<double> SortedKeys => sortedKeys;
-        readonly CubicFunction[] functions;
-        public ReadOnlySpan<CubicFunction> Functions => functions;
-        public double Duration => sortedKeys[^1];
-
-        Spline(double[] sortedKeys, CubicFunction[] functions)
+        struct float9
         {
-            this.sortedKeys = sortedKeys;
-            this.functions = functions;
+            public double a;
+            public double b;
+            public double c;
+            public double d;
+            public double e;
+            public double f;
+            public double g;
+            public double h;
+            public double i;
+
+            public static float9 LeftValue(double t, double v)
+            {
+                return new float9()
+                {
+                    a = t * t * t,
+                    b = t * t,
+                    c = t,
+                    d = 1f,
+                    i = v,
+                };
+            }
+            public static float9 Velocity(double t)
+            {
+                return new float9()
+                {
+                    a = 3f * t * t,
+                    b = 2f * t,
+                    c = 1f,
+                    d = 0f,
+                    e = -3f * t * t,
+                    f = -2f * t,
+                    g = -1f,
+                    h = 0f,
+                    i = 0f,
+                };
+            }
+            public static float9 Acceleration(double t)
+            {
+                return new float9()
+                {
+                    a = 6f * t,
+                    b = 2f,
+                    c = 0f,
+                    d = 0f,
+                    e = -6f * t,
+                    f = -2f,
+                    g = 0f,
+                    h = 0f,
+                    i = 0f,
+                };
+            }
+            public float9 AddScale(float9 src, double scale)
+            {
+                return new float9()
+                {
+                    a = a + src.a * scale,
+                    b = b + src.b * scale,
+                    c = c + src.c * scale,
+                    d = d + src.d * scale,
+                    e = e + src.e * scale,
+                    f = f + src.f * scale,
+                    g = g + src.g * scale,
+                    h = h + src.h * scale,
+                    i = i + src.i * scale,
+                };
+            }
+            public double4 Left
+            {
+                get => new double4(a, b, c, d);
+                set
+                {
+                    a = value.x;
+                    b = value.y;
+                    c = value.z;
+                    d = value.w;
+                }
+            }
+
+            public double4 Right
+            {
+                get => new double4(e, f, g, h);
+                set
+                {
+                    e = value.x;
+                    f = value.y;
+                    g = value.z;
+                    h = value.w;
+                }
+            }
+            public double Value
+            {
+                get => i;
+                set => i = value;
+            }
         }
-        public static Spline SolvePoints(params (double time, double value)[] points)
+
+        readonly CubicFunction[] intervals;
+        public System.ReadOnlySpan<CubicFunction> Intervals => intervals;
+        readonly float[] endTimes;
+        public System.ReadOnlySpan<float> EndTimes => endTimes;
+        public float Duration => endTimes[^1];
+
+        public Spline(params (float time, float value)[] values)
         {
-            if (points.Length == 1)
+            if (values.Length <= 1)
             {
-                return new Spline(
-                    new[] { points[0].time },
-                    new[] { CubicFunction.Constant(points[0].value) });
-            }
-            if (points.Length == 2)
-            {
-                var func = CubicFunction.Solve2Points(points[0], points[1]);
-                return new Spline(
-                    new[] { points[1].time },
-                    new[] { func });
-            }
-            if (points.Length == 3)
-            {
-                var func = CubicFunction.Solve3Points(points[0], points[1], points[2]);
-                return new Spline(
-                    new[] { points[2].time },
-                    new[] { func });
+                throw new System.ArgumentException();
             }
 
-            var times = points.Select(_ => _.time).ToArray();
-            var current = CubicFunction.Solve4Points(points[0], points[1], points[2], points[3]);
-            var intervals = new List<(double endTime, CubicFunction func)>();
-            intervals.Add((points[3].time, current));
+            intervals = new CubicFunction[values.Length - 1];
+            endTimes = values.Skip(1).Select(_ => _.time).ToArray();
 
-            for (int i = 3; i < points.Length - 1; i++)
+            // 最初の加速は0とする.6ax + 2b = 0
+            var current = new float9()
             {
-                var start = points[i];
-                var end = points[i + 1];
-                var startV = current.Velocity(start.time);
-                var startA = current.Acceleration(start.time);
-                current = CubicFunction.Solve2PointsVelocityAndAccel(start, end, (start.time, startV), (start.time, startA));
-                intervals.Add((end.time, current));
-            }
-            var sortedKeys = intervals.Select(_ => _.endTime).ToArray();
-            var functions = intervals.Select(_ => _.func).ToArray();
+                Right = new double4(0f, 2f, 0f, 0f),
+                Value = 0f,
+            };
 
-            return new Spline(sortedKeys, functions);
+            var trails = new List<(double4 left, double right)>();
+            for (int i = 0; i < values.Length - 1; ++i)
+            {
+                var start = values[i];
+                var end = values[i + 1];
+                var a = float9.LeftValue(start.time, start.value);
+                var b = float9.LeftValue(end.time, end.value);
+                var c = float9.Acceleration(end.time);
+                var d = float9.Velocity(end.time);
+                current = Solve(current, a, b, c, d);
+                trails.Add((current.Left, current.Value));
+            }
+
+            // 最後の加速は0
+            var trail = (left: new double4(x: 6.0 * values[^1].time, y: 2.0, z: 0.0, w: 0.0), right: 0.0);;
+            var trail2 = (left: current.Left, right: current.Value);
+
+            for (int i = 0; i < values.Length - 1; ++i)
+            {
+                var index = values.Length - i - 1;
+                var t1 = values[index].time;
+                var t2 = values[index - 1].time;
+                //var trail2 = trails[index - 1];
+
+                var matrix = new double4x4(
+                    m00: t1 * t1 * t1,
+                    m01: t1 * t1,
+                    m02: t1,
+                    m03: 1f,
+
+                    m10: t2 * t2 * t2,
+                    m11: t2 * t2,
+                    m12: t2,
+                    m13: 1f,
+
+                    m20: trail.left.x,
+                    m21: trail.left.y,
+                    m22: trail.left.z,
+                    m23: trail.left.w,
+
+                    m30: trail2.left.x,
+                    m31: trail2.left.y,
+                    m32: trail2.left.z,
+                    m33: trail2.left.w
+                );
+
+                var right = new double4();
+                right.x = values[index].value;
+                right.y = values[index - 1].value;
+                right.z = trail.right;
+                right.w = trail2.right;
+                Debug.Log(right);
+
+                var inversedMatrix = math.inverse(matrix);
+                var k = math.mul(inversedMatrix, right);
+                var interval = new CubicFunction((float)k.x, (float)k.y, (float)k.z, (float)k.w);
+
+                UnityEngine.Assertions.Assert.IsFalse(float.IsNaN(interval.a), trail.right.ToString());
+                UnityEngine.Assertions.Assert.IsFalse(float.IsNaN(interval.b));
+                UnityEngine.Assertions.Assert.IsFalse(float.IsNaN(interval.c));
+                UnityEngine.Assertions.Assert.IsFalse(float.IsNaN(interval.d));
+
+                intervals[index - 1] = interval;
+                var startTime = values[index - 1].time;
+                trail = (new double4(6d * startTime, 2d, 0d, 0d), interval.Acceleration(startTime));
+                trail2 = (new double4(3d * startTime * startTime, 2d * startTime, 1d, 0f), interval.Velocity(startTime));
+                Debug.Log(matrix);
+                Debug.Log(inversedMatrix);
+                Debug.Log(right);
+                Debug.Log((interval.a, interval.b, interval.c, interval.d));
+            }
         }
 
-        public static Spline SolveAccelAndVelocityAndPoints(double t, double a, double v, params (double time, double value)[] points)
+        public float Evaluate(float t)
         {
-            var times = points.Select(_ => _.time).ToArray();
-            var index = Array.BinarySearch(times, t);
+            return Get(t).Evaluate(t);
+        }
+
+        public float Velocity(float t)
+        {
+            return Get(t).Velocity(t);
+        }
+
+        public float Acceleration(float t)
+        {
+            return Get(t).Acceleration(t);
+        }
+
+        public CubicFunction Get(float t)
+        {
+            var index = System.Array.BinarySearch(endTimes, t);
             if (index < 0)
             {
                 index = ~index;
             }
-            if (index >= times.Length - 1)
+            if (index >= endTimes.Length)
             {
-                index = times.Length - 2;
+                index = endTimes.Length - 1;
             }
-            var pivot = CubicFunction.Solve2PointsVelocityAndAccel(points[index], points[index + 1], (t, v), (t, a));
+            return intervals[index];
+        }
 
-            var intervals = new List<(double endTime, CubicFunction func)>();
-            intervals.Add((points[index + 1].time, pivot));
-
+        float9 Solve(
+            float9 a,
+            float9 b,
+            float9 c,
+            float9 d,
+            float9 e)
+        {
+            var list = new List<float9>() { a, b, c, d, e };
             {
-                CubicFunction current = pivot;
-                for (int i = index + 1; i < times.Length - 1; ++i)
+                var x = list.Where(_ => _.d != 0f).ToArray();
+                list.RemoveAll(_ => _.d != 0f);
+                for (int i = 1; i < x.Length; ++i)
                 {
-                    var start = points[i];
-                    var end = points[i + 1];
-                    var startV = current.Velocity(start.time);
-                    var startA = current.Acceleration(start.time);
-
-                    current = CubicFunction.Solve2PointsVelocityAndAccel(start, end, (start.time, startV), (start.time, startA));
-                    intervals.Add((end.time, current));
+                    var p = x[0];
+                    var q = x[i];
+                    var f = p.AddScale(q, -p.d / q.d);
+                    f.d = 0f;
+                    list.Add(f);
                 }
             }
-
             {
-                CubicFunction current = pivot;
-                for (int i = 0; i < index; ++i)
+                var x = list.Where(_ => _.c != 0f).ToArray();
+                list.RemoveAll(_ => _.c != 0f);
+                for (int i = 1; i < x.Length; ++i)
                 {
-                    var j = index - i - 1;
-                    var start = points[j];
-                    var end = points[j + 1];
-                    var endV = current.Velocity(end.time);
-                    var endA = current.Acceleration(end.time);
-
-                    current = CubicFunction.Solve2PointsVelocityAndAccel(start, end, (end.time, endV), (end.time, endA));
-                    intervals.Insert(0, (end.time, current));
+                    var p = x[0];
+                    var q = x[i];
+                    var f = p.AddScale(q, -p.c / q.c);
+                    f.c = 0f;
+                    list.Add(f);
                 }
             }
-
-            var sortedKeys = intervals.Select(_ => _.endTime).ToArray();
-            var functions = intervals.Select(_ => _.func).ToArray();
-
-            return new Spline(sortedKeys, functions);
-        }
-
-        public static Spline SolvedVelocityAndPoints(double t, double v, params (double time, double value)[] points)
-        {
-            var times = points.Select(_ => _.time).ToArray();
-            var index = Array.BinarySearch(times, t);
-            if (index < 0)
             {
-                index = ~index;
-            }
-            if (index >= times.Length - 1)
-            {
-                index = times.Length - 2;
-            }
-            var pivot = CubicFunction.Solve2PointsAndVelocity(points[index], points[index + 1], (t, v));
-
-            var intervals = new List<(double endTime, CubicFunction func)>();
-            intervals.Add((points[index + 1].time, pivot));
-
-            {
-                CubicFunction current = pivot;
-                for (int i = index + 1; i < times.Length - 1; ++i)
+                var x = list.Where(_ => _.b != 0f).ToArray();
+                list.RemoveAll(_ => _.b != 0f);
+                for (int i = 1; i < x.Length; ++i)
                 {
-                    var start = points[i];
-                    var end = points[i + 1];
-                    var startV = current.Velocity(start.time);
-                    var startA = current.Acceleration(start.time);
-
-                    current = CubicFunction.Solve2PointsVelocityAndAccel(start, end, (start.time, startV), (start.time, startA));
-                    intervals.Add((end.time, current));
+                    var p = x[0];
+                    var q = x[i];
+                    var f = p.AddScale(q, -p.b / q.b);
+                    f.b = 0f;
+                    list.Add(f);
                 }
             }
-
             {
-                CubicFunction current = pivot;
-                for (int i = 0; i < index; ++i)
+                var x = list.Where(_ => _.a != 0f).ToArray();
+                list.RemoveAll(_ => _.a != 0f);
+                for (int i = 1; i < x.Length; ++i)
                 {
-                    var j = index - i - 1;
-                    var start = points[j];
-                    var end = points[j + 1];
-                    var endV = current.Velocity(end.time);
-                    var endA = current.Acceleration(end.time);
-
-                    current = CubicFunction.Solve2PointsVelocityAndAccel(start, end, (end.time, endV), (end.time, endA));
-                    intervals.Insert(0, (end.time, current));
+                    var p = x[0];
+                    var q = x[i];
+                    var f = p.AddScale(q, -p.a / q.a);
+                    f.a = 0f;
+                    list.Add(f);
                 }
             }
+            UnityEngine.Assertions.Assert.AreEqual(1, list.Count);
 
-            var sortedKeys = intervals.Select(_ => _.endTime).ToArray();
-            var functions = intervals.Select(_ => _.func).ToArray();
-
-            return new Spline(sortedKeys, functions);
-        }
-
-        public double Evaluate(double t)
-        {
-            return functions[GetIntervalIndex(t)].Evaluate(t);
-        }
-
-        public double Velocity(double t)
-        {
-            return functions[GetIntervalIndex(t)].Velocity(t);
-        }
-
-        public double Acceleration(double t)
-        {
-            return functions[GetIntervalIndex(t)].Acceleration(t);
-        }
-
-        int GetIntervalIndex(double t)
-        {
-            var index = System.Array.BinarySearch(sortedKeys, t);
-            if (index < 0)
+            UnityEngine.Assertions.Assert.IsFalse(double.IsNaN(list[0].Value));
+            UnityEngine.Assertions.Assert.IsFalse(double.IsNaN(list[0].e));
+            UnityEngine.Assertions.Assert.IsFalse(double.IsNaN(list[0].f));
+            UnityEngine.Assertions.Assert.IsFalse(double.IsNaN(list[0].g));
+            UnityEngine.Assertions.Assert.IsFalse(double.IsNaN(list[0].h));
+            return new float9()
             {
-                index = ~index;
-            }
-            if (index >= sortedKeys.Length)
-            {
-                index = sortedKeys.Length - 1;
-            }
-            return index;
+                Left = list[0].Right,
+                Value = list[0].Value,
+            };
         }
     }
 }
+

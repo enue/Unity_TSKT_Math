4ea516717356959edcc3cb7c13050d6fefac2480
enue <otoriden@gmail.com>
2025-04-08 07:38:50 +0900

    Vector2Util.Rotate

--- Assets/Package/Runtime/Vector2Util.cs
+++ Assets/Package/Runtime/Vector2Util.cs
@@ -1,5 +1,6 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using Unity.Mathematics;
 using UnityEngine;
 #nullable enable
 
@@ -7,12 +8,22 @@ namespace TSKT
 {
     public static class Vector2Util
     {
-        static public Vector2 Rotate(Vector2 source, float radian)
+        public static Vector2 Rotate(Vector2 source, float radian)
         {
             var sin = Mathf.Sin(radian);
             var cos = Mathf.Cos(radian);
 
             return new Vector2(
+                cos * source.x - sin * source.y,
+                sin * source.x + cos * source.y);
+        }
+
+        public static float2 Rotate(in float2 source, float radian)
+        {
+            var sin = math.sin(radian);
+            var cos = math.cos(radian);
+
+            return new float2(
                 cos * source.x - sin * source.y,
                 sin * source.x + cos * source.y);
         }
